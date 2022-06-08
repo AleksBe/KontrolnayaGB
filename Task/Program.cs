63db7fe373743cb90d061caecaf53844521ec851
AleksBe <103070433+AleksBe@users.noreply.github.com>
2022-06-08 20:05:03 +0300

    Создали метод CorrectArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,11 @@
 ﻿string[]array = {"Москва", "Владивосток", "Уфа", "Коля", "Оля", "уж", ":-)"};
 
+void CorrectArray(string[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length > 3) array[i] = string.Empty;
+    }
+}
+
 
